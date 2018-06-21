3c3e1bfaed530759217b452c3e4690d5b338516f
RANMENGYA <ranmengya@163.com>
2018-06-21 10:09:28 +0800

    项目初始化

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class="container">
+   <h2>登录页</h2>
+ </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped lang="less">
+</style>
